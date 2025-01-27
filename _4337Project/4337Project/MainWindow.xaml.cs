44068d690edfe59aade32e9467090aa363a8e925
ezy4me <mrronal@mail.ru>
2025-01-27 12:25:37 +0300

    Revert "Merge pull request #2 from Focsterrr/main"
    This reverts commit b6de34936dcae586e0180492d418dbfdd983dce9, reversing
    changes made to 56f1837467ca57e6272cff7a24823185bee06780.

--- _4337Project/4337Project/MainWindow.xaml.cs
+++ _4337Project/4337Project/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace _4337Project
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            _4337_Sergeev secondWindow = new _4337_Sergeev();
+            _4337_d0h secondWindow = new _4337_d0h();
 
             // Show the window
             secondWindow.Show();
--- _4337Project/4337Project/_4337_d0h.xaml.cs
+++ _4337Project/4337Project/_4337_d0h.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace _4337Project
 {
     /// <summary>
-    /// Логика взаимодействия для _4337_Sergeev.xaml
+    /// Interaction logic for _4337_d0h.xaml
     /// </summary>
-    public partial class _4337_Sergeev : Window
+    public partial class _4337_d0h : Window
     {
-        public _4337_Sergeev()
+        public _4337_d0h()
         {
             InitializeComponent();
         }
